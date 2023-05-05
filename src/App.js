a2907fc87a0ca8cc142b4c264a13e56b39476ac0
jvictort <jvteodorobrasil@gmail.com>
2023-05-04 21:55:42 -0300

    Adição de estilo global e seção Login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import GlobalStyle from './styles/GlobalStyles';
+
+import Login from './Login/index';
 
 const App = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch("http://localhost:8080/", {
-      method: 'GET',
-      mode: 'cors'
-    })
-    .then(res => res.json())
-    .then(res => setMessage(res.message));
-
-  }, []);
-
   return (
     <>
-      <h1>{message}</h1>
+      <GlobalStyle />
+      <Login />
     </>
   );
 }
