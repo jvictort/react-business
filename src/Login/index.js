04a6f957d66207866c63fb2361b7e20dd9712351
jvictort <jvteodorobrasil@gmail.com>
2023-05-07 23:51:38 -0300

    Implementação dos componentes de formulário

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as C from './style';
 import { FaBusinessTime } from 'react-icons/fa';
 
+import RegisterForm from './components/RegisterForm/index';
 import LoginForm from './components/LoginForm/index';
 
+const apiUrl = 'http://localhost:8080';
+
 const Login = () => {
+  const [isLoginForm, setLoginForm] = useState(false);
+
   return (
     <C.MainContainer>
       <C.MainTitle>React Business <FaBusinessTime /></C.MainTitle>
-      <LoginForm />
+      {!isLoginForm ? (
+        <RegisterForm setLoginForm={setLoginForm} url={apiUrl}/>
+      ) : (
+        <LoginForm setLoginForm={setLoginForm} url={apiUrl}/>
+      )}
     </C.MainContainer>
   );
 }
