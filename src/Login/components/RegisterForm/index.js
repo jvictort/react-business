16cffd846fc7e895dfb2279addf2d14273c176bf
jvictort <jvteodorobrasil@gmail.com>
2023-05-07 23:50:41 -0300

    Criação do componente de formulário de cadastro

---
+++ src/Login/components/RegisterForm/index.js
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+
+import * as C from './style';
+
+import {
+  FaUserAlt,
+  FaMailBulk,
+  FaLock
+} from 'react-icons/fa';
+
+const RegisterForm = ({ setLoginForm, url }) => {
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [repeatedPassword, setRepeatedPassword] = useState('');
+  const [isValidPassword, setValidPassword] = useState(false);
+
+  useEffect(() => {
+    setValidPassword(userPassword === repeatedPassword ? true : false);
+  }, [userPassword, repeatedPassword])
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    if(userPassword === repeatedPassword) {
+      const user = {
+        userName,
+        userEmail,
+        userPassword,
+        repeatedPassword
+      }
+
+      fetch(url + '/user/add', {
+        method: 'POST',
+        mode: 'cors',
+        body: JSON.stringify(user),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      });
+    }
+  }
+
+  return (
+    <C.FormContainer onSubmit={handleSubmit}>
+      <C.FormFieldset>
+        <C.FormLegend>Cadastro</C.FormLegend>
+
+        <C.InputContainer>
+          <FaUserAlt />
+
+          <C.FormInput
+            placeholder="Nome"
+            required
+            onChange={event => setUserName(event.target.value)}
+          />
+        </C.InputContainer>
+
+        <C.InputContainer>
+          <FaMailBulk />
+
+          <C.FormInput
+            placeholder="Email"
+            type="email"
+            required
+            onChange={event => setUserEmail(event.target.value)}
+          />
+        </C.InputContainer>
+
+        <C.InputContainer>
+          <FaLock />
+
+          <C.FormInput
+            placeholder="Senha"
+            type="password"
+            onChange={event => setUserPassword(event.target.value)}
+          />
+        </C.InputContainer>
+
+        <C.InputContainer>
+          <FaLock />
+
+          <C.FormInput
+            placeholder="Reinsira a senha"
+            type="password"
+            onChange={event => setRepeatedPassword(event.target.value)}
+          />
+        </C.InputContainer>
+
+        {(userPassword !== '' || repeatedPassword !== '') ? (
+          <C.ValidPassword color={!isValidPassword ? 'red' : 'green'}>
+            {!isValidPassword ? 'As senhas estão diferentes' : 'As senhas são iguais'}
+          </C.ValidPassword>
+        ) : null}
+
+        <C.FormOption>
+          Já possui uma conta? Faça login clicando
+          <span onClick={() => setLoginForm(true)}> aqui </span>
+        </C.FormOption>
+
+        <C.FormButton type="submit">Criar conta</C.FormButton>
+      </C.FormFieldset>
+    </C.FormContainer>
+  );
+}
+
+export default RegisterForm;
---
+++ src/Login/components/RegisterForm/style.js
@@ -0,0 +1,87 @@
+import styled from 'styled-components';
+
+export const FormContainer = styled.form`
+  padding: 1em;
+  background: linear-gradient(rgba(0,0,0,.9),rgba(0,0,0,.9));
+  border-radius: 0.3em;
+`;
+
+export const FormFieldset = styled.fieldset`
+  padding: 1em;
+  border: 2px solid #0179c6;
+  border-radius: 0.3em;
+`;
+
+export const FormLegend = styled.legend`
+  background-color: #0179c6;
+  padding: 0.5em;
+  border-radius: 0.5em;
+  font-size: 0.9em;
+  font-weight: 500;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid white;
+  margin-top: 2.5em;
+  color: white;
+  cursor: pointer;
+
+  svg {
+    margin-right: 0.5em;
+    transition: color 0.3s;
+  }
+
+  &:hover {
+    color: #0179c6;
+  }
+`;
+
+export const FormInput = styled.input`
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  color: white;
+  outline: none;
+`;
+
+export const ValidPassword = styled.p`
+  font-size: 0.8em;
+  text-align: center;
+  color: ${(props) => props.color};
+  margin-top: 0.3em;
+`;
+
+export const FormOption = styled.p`
+  font-size: 0.8em;
+  text-align: center;
+  margin-top: 1.5em;
+  color: white;
+
+  span {
+    color: #0179c6;
+    cursor: pointer;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #005489;
+    }
+  }
+`;
+
+export const FormButton = styled.button`
+  width: 100%;
+  padding: 0.7em;
+  margin-top: 1em;
+  border-radius: 0.5em;
+  border: none;
+  background-color: #0179c6;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #005489;
+  }
+`;
